7a502699d20fe9b7fe8228760e440488c2d2f126
luca sguanci <luca.sguanci@gmail.com>
2025-01-29 14:58:12 +0100

    minor changes

--- js/basic.js
+++ js/basic.js
@@ -8,12 +8,13 @@ window.onload = () => {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
             // Add a box to the north of the initial GPS position
             const entity = document.createElement("a-box");
+            entity.setAttribute('class','box');
             entity.setAttribute("geometry", {
-                width: 2, 
-                height: 2,
-                depth: 2
+                "width": 100, 
+                "height": 50,
+                "depth": 17
             });
-            entity.setAttribute('material', { color: 'red' } );
+            entity.setAttribute('material', { "color": 'green' } );
             entity.setAttribute('gps-new-entity-place', {
                 latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude,
