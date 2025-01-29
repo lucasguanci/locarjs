215538f5dbe807e647f42aa5d6677fe3fa54a81e
luca sguanci <luca.sguanci@gmail.com>
2025-01-29 15:15:59 +0100

    minor changes

--- js/basic.js
+++ js/basic.js
@@ -7,20 +7,30 @@ window.onload = () => {
         if(!testEntityAdded) {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
             // Add a box to the north of the initial GPS position
-            const entity = document.createElement("a-box");
-            entity.setAttribute('class','box');
-            entity.setAttribute("geometry", {
+            const box = document.createElement("a-box");
+            box.setAttribute('class','box');
+            box.setAttribute("geometry", {
                 "width": 100, 
                 "height": 50,
                 "depth": 17
             });
-            entity.setAttribute('material', { "color": 'green' } );
-            entity.setAttribute('gps-new-entity-place', {
+            box.setAttribute('material', { "color": 'green' } );
+            box.setAttribute('gps-new-entity-place', {
                 latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude,
                 position: '0 5 10' // added some elevation
             });
-            document.querySelector("a-scene").appendChild(entity);
+            const sphere = document.createElement("a-sphere");
+            sphere.setAttribute("class","sphere");
+            sphere.setAttribute("radius", 10);
+            sphere.setAttribute('material', { "color": 'cyan' });
+            sphere.setAttribute('gps-new-entity-place', {
+                latitude: e.detail.position.latitude + 0.001,
+                longitude: e.detail.position.longitude
+            });
+            sphere.setAttribute("position", "300 100 200")
+            document.querySelector("a-scene").appendChild(box);
+            document.querySelector("a-scene").appendChild(sphere);
         }
         testEntityAdded = true;
     });
