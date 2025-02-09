ec37ce470cb934e62354da45362e1f6de0ae5da6
luca sguanci <luca.sguanci@gmail.com>
2025-02-09 06:48:14 +0100

    w/button version

--- src/main.js
+++ src/main.js
@@ -5,48 +5,46 @@
 import * as THREE from 'three';
 import * as LocAR from 'locar';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.001, 100);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+var startBtn = document.getElementById("startBtn");
+startBtn.addEventListener('click', e => {
+  init();
+});
 
-window.addEventListener("resize", e => {
+function init() {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.001, 100);
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  window.addEventListener("resize", e => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;    
     camera.updateProjectionMatrix();
-});
-const box = new THREE.BoxGeometry(2,2,2);
-const cube = new THREE.Mesh(box, new THREE.MeshBasicMaterial({ color: 0xff0000 }));
+  });
+  const box = new THREE.BoxGeometry(2,2,2);
+  const cube = new THREE.Mesh(box, new THREE.MeshBasicMaterial({ color: 0xff0000 }));
 
-const locar = new LocAR.LocationBased(scene, camera);
-const cam = new LocAR.WebcamRenderer(renderer);
+  const locar = new LocAR.LocationBased(scene, camera);
+  const cam = new LocAR.WebcamRenderer(renderer);
 
-// Create the device orientation tracker
-const deviceOrientationControls = new LocAR.DeviceOrientationControls(camera);
-deviceOrientationControls.disconnect()
-deviceOrientationControls.connect()
+  // Create the device orientation tracker
+  const deviceOrientationControls = new LocAR.DeviceOrientationControls(camera);
 
-console.log(locar);
+  locar.startGps();
 
-locar.startGps();
-console.log(locar.initialPosition)
+  locar.add(cube, -0.72, 51.0501);
 
-console.log(cube);
-
-locar.add(cube, -0.72, 51.0501);
-
-console.log("ecco il cubo "+cube);
-
-renderer.setAnimationLoop(animate);
+  renderer.setAnimationLoop(animate);    
+}
 
 
 function animate() {
-    // Update the scene using the latest sensor readings
-    deviceOrientationControls.update();
+  // Update the scene using the latest sensor readings
+  deviceOrientationControls.update();
 
-    cam.update();
-    renderer.render(scene, camera);
+  cam.update();
+  renderer.render(scene, camera);
 }
 
 
