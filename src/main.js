1456dfaed2e83063c2e45e82dbf803b5177bb920
luca sguanci <luca.sguanci@gmail.com>
2025-02-17 10:46:21 +0100

    changed starting comments

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 /**
  * LocAR.js Tutorial - Part 2
  * https://github.com/AR-js-org/locar.js/blob/master/docs/tutorial/part2.md
+ * modified to enable device orientation on ios(btnStart)
+ * modified to use webkit absoluteDeviceOrientationControl, see https://github.com/AR-js-org/AR.js/issues/466
+ * see also https://github.com/mrdoob/three.js/blob/1ee2fca970e3afdc26e6c2a47c14e9e2b784ae48/examples/jsm/controls/DeviceOrientationControls.js
+ * for calling this.connect()
 **/
 import * as THREE from 'three';
 const isIOS = navigator.userAgent.match(/iPhone|iPad|iPod/i)
